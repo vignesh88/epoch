ed0ffff9826e03399dfff3fe22d3fff5534bbeca
vignesh <me@vikki.in>
2020-05-16 18:56:23 +0530

    Adding git ignore

--- epoch_converter/tests.py
+++ epoch_converter/tests.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import Client
+# create an instance of the client for our use
+client = Client()
+
+class QuestionModelTests(TestCase):
+
+    def test_default_page(self):
+        response = client.get('/')
+        
+        #self.assertIs(future_question.was_published_recently(), False)
+        self.assertEqual(response.status_code, 200)
